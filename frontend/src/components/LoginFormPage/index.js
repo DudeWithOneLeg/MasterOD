fa7c052ac70a1d3c1658563f68f93d9b8cfdb604
dudewithoneleg <galvancromeo@gmail.com>
2024-03-30 18:23:55 -0500

    login styling

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -26,33 +26,31 @@
       };
 
       return (
-        <>
-          <h1>Log In</h1>
-          <form onSubmit={handleSubmit}>
-            <label>
-              Username or Email
+        <div className="w-full flex flex-col items-center justify-content-center">
+          {/* <h1>Log In</h1> */}
+          <form onSubmit={handleSubmit} className="flex flex-col w-1/6 rounded border bg-slate-700 p-3">
               <input
                 type="text"
                 value={credential}
                 onChange={(e) => setCredential(e.target.value)}
                 required
+                placeholder='Username or Email'
+                className="rounded my-1.5 p-1"
               />
-            </label>
-            <label>
-              Password
               <input
                 type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
+                placeholder="Password"
+                className="rounded p-1"
               />
-            </label>
-            {errors.credential && <p>{errors.credential}</p>}
-            <button type="submit">Log In</button>
+            {errors.credential && <p className="text-red-300 py-1.5">{errors.credential}</p>}
+            <button type="submit" className="text-slate-200 border rounded hover:bg-slate-500 py-1 my-2">Log In</button>
+          <button className="text-slate-200 border rounded hover:bg-slate-500 py-1">Sign up</button>
           </form>
-        </>
+        </div>
       );
     }
 
     export default LoginFormPage;
-                  
--- frontend/src/components/SideBar/index.js
+++ frontend/src/components/SideBar/index.js
@@ -1,5 +1,19 @@
 export default function SideBar() {
-    return <div className="h-full w-1/5 bg-slate-400 overflow-hidden">
-
+  return (
+    <div className="h-full w-1/5 bg-slate-500 overflow-hidden text-slate-200">
+      <div className="p-4">
+        <div className="flex flex-row items-center">
+          <img src="icons/profile.jpg" className="rounded-full h-14"></img>
+          <p className="pl-4">User Name</p>
+        </div>
+        <div className="p-4 text-lg divide-y">
+            <p className="p-2">Queries</p>
+            <p className="p-2">Recent Queries</p>
+            <p className="p-2">Visited</p>
+            <p className="p-2">Saved Sites</p>
+            <p className="p-2">Site History</p>
+        </div>
+      </div>
     </div>
+  );
 }
