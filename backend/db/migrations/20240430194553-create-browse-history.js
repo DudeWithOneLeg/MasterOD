44253e2a6dbed1196ee1ad4f47048c718355d6fb
dudewithoneleg <galvancromeo@gmail.com>
2024-04-30 15:06:54 -0500

    added browse history table

---
+++ backend/db/migrations/20240430194553-create-browse-history.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+const options = {}
+if (process.env.NODE_ENV === 'production') {
+  options.process.env.SHEMA
+}
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('BrowseHistories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      queryId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'BrowseHistories'
+    await queryInterface.dropTable(options);
+  }
+};
