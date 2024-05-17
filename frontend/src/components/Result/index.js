c329d30a2c90827677e5b058a9e13512e4252a7d
dudewithoneleg <galvancromeo@gmail.com>
2024-05-17 04:41:33 -0500

    fixed result frontend

--- frontend/src/components/Result/index.js
+++ frontend/src/components/Result/index.js
@@ -1,7 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import * as resultActions from '../../store/result'
-
+import * as resultActions from "../../store/result";
 
 export default function Result({
   data,
@@ -9,36 +8,38 @@ export default function Result({
   showResult,
   setShowResult,
   setPreview,
-  setResult
+  setResult,
 }) {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [showInfo, setShowInfo] = useState(false);
   const [saved, setSaved] = useState(false);
-  const lastSearchId = useSelector(state => Object.values(state.search.recentQueries)[0].id)
+  const lastSearchId = useSelector(
+    (state) => Object.values(state.search.recentQueries)[0].id
+  );
 
   const docExtensions = ["pdf", "doc", "docx"];
+  const result = data[rowKey];
 
   const handlClick = () => {
-    const newResult = {...data[rowKey]}
-    newResult.queryId = lastSearchId
+    const newResult = { ...data[rowKey] };
+    newResult.queryId = lastSearchId;
     setShowInfo(!showInfo);
     setShowResult(true);
-    setResult(newResult)
+    setResult(newResult);
     setPreview(data[rowKey].link);
   };
 
   const saveResult = () => {
-    const result = data[rowKey]
     const newResult = {
       title: result.title,
-      description: result.snippet,
-      url: result.link,
-      queryId: lastSearchId
-    }
+      snippet: result.snippet,
+      link: result.link,
+      queryId: lastSearchId,
+    };
 
-    dispatch(resultActions.postSavedResult(newResult))
-    setSaved(true)
-  }
+    dispatch(resultActions.postSavedResult(newResult));
+    setSaved(true);
+  };
 
   return (
     <div
@@ -50,61 +51,45 @@ export default function Result({
     >
       <div className=""></div>
       <div className="flex flex-col items-center justify-content-around min-w-10 h-full">
-        {saved ? <img src='/icons/bookmark_FILL.png'/> : <img
-        onClick={saveResult}
-        src="/icons/bookmark.png" />}
+        {saved ? (
+          <img src="/icons/bookmark_FILL.png" />
+        ) : (
+          <img onClick={saveResult} src="/icons/bookmark.png" />
+        )}
         <div className="flex font-bold h-fit w-fit bg-slate-300 rounded"></div>
       </div>
       <div
         onClick={handlClick}
         className="flex flex-col items-start h-full w-full"
       >
-        {data[rowKey] &&
-          Object.keys(data[rowKey])
-            .slice(1)
-            .map((columnKey) => (
-              <div
-                key={columnKey}
-                className={`flex flex-col text-slate-400 h-fit`}
-              >
-                {!data[rowKey][columnKey]?.url && columnKey != "title" ? (
-                  // <div className="flex flex-wrap h-fit">
-                  <div>
-                    <p
-                      className={
-                        columnKey == "link"
-                          ? "text-sm text-slate-300 flex w-full"
-                          : "underline"
-                      }
-                    >
-                      {data[rowKey][columnKey]}
-                    </p>
+        {result && (
+          <div key={result.id} className={`flex flex-col text-slate-400 h-fit`}>
+            <div className="flex flex-row ">
+              <h3 className="font-bold text-slate-300 text-2xl text-wrap underline w-full">
+                {result.title}
+              </h3>
+              {docExtensions.includes(result.link.split(".").slice(-1)[0]) && (
+                <img src="images/document.png" className="w-8 h-8" />
+              )}
+            </div>
+            <div>
+              <p className={"underline"}>{result.snippet}</p>
+            </div>
+            <div>
+              <p className={"text-sm text-slate-300 flex w-full"}>
+                {result.createdAt}
+              </p>
+            </div>
 
-                  </div>
-                ) : //</div>
-                data[rowKey][columnKey].archived_snapshots ? (
-                  <a
-                    href={
-                      data[rowKey][columnKey].archived_snapshots.closest?.url
-                    }
-                    target="_blank"
-                    className="font-bold text-slate-400"
-                  >
-                    Archive
-                  </a>
-                ) : (
-                  <div className="flex flex-row ">
-
-                  <h3 className="font-bold text-slate-300 text-2xl text-wrap underline w-full">
-                    {data[rowKey][columnKey]}
-                  </h3>
-                  {docExtensions.includes(
-                      data[rowKey].link.split(".").slice(-1)[0]
-                    ) && <img src="images/document.png" className="w-8 h-8" />}
-                  </div>
-                )}
-              </div>
-            ))}
+            {result?.archived_snapshots?.closest?.url && <a
+              href={result.archived_snapshots.closest.url}
+              target="_blank"
+              className="font-bold text-slate-400"
+            >
+              Archive
+            </a>}
+          </div>
+        )}
       </div>
     </div>
   );
