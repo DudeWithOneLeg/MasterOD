04fa161353ebaf408ccea3a265f4fb5c6655f705
dudewithoneleg <galvancromeo@gmail.com>
2024-08-21 20:59:37 -0500

    add browser archive buttons

--- frontend/src/components/Browser/index.js
+++ frontend/src/components/Browser/index.js
@@ -9,6 +9,7 @@ export default function Browser({
 }) {
   const domRef = useRef(null);
   const [showArchive, setShowArchive] = useState(false);
+  const [showAnalyze, setShowAnalyze] = useState(false);
 
   const docExtensions = ["pdf", "ppt", "doc", "docx"];
   const msOfficeDocs = ["ppt", "doc", "docx"];
@@ -34,7 +35,7 @@ export default function Browser({
       className="flex flex-col h-full w-full bg-slate-300 ml-2 p-1 rounded overflow-none"
       // ref={parentRef}
     >
-      {/* <div className="fixed -mt-6 flex flex-row text-white pb-1">
+      <div className="fixed -mt-6 flex flex-row text-white pb-1">
         <div
           className={`bg-slate-${!showArchive ? '400' : '600 hover:bg-slate-400'} rounded mx-1 px-1 cursor-pointer`}
           onClick={() => setShowArchive(false)}
@@ -47,7 +48,7 @@ export default function Browser({
         >
           Archive
         </div>
-      </div> */}
+      </div>
       <div className="bg-slate-400 flex flex-row items-center justify-content-between w-full p-2 h-[5%]">
         <div className="w-full flex flex-row justify-content-between h-8 bg-slate-100 p-1 rounded">
           <p className="w-full truncate rounded">{preview}</p>
